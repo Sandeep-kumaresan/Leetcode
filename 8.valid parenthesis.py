5a2783b44a7adbc9713b1a9f12f9c8e418fc1f27
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-08-13 16:26:27 +0530

    trying

---
+++ 8.valid parenthesis.py
@@ -0,0 +1,26 @@
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    # count=0
+    # count2=0
+    # count3=0
+    # for i in s:
+    #     if i == '[':
+    #         count += 1
+    #     elif i == ']':
+    #         count -= 1
+    #     elif i == '{':
+    #         count2 += 1
+    #     elif i == '}':
+    #         count2 -= 1
+    #     elif i == '(':
+    #         count3 += 1
+    #     elif i == ')':
+    #         count3 -= 1
+    # if count == 0 and count2 == 0 and count3 == 0:
+    #     return True
+    # else:
+    #     return False
+
