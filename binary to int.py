fd56b8f1730f5cf70b67b43e46f3ec1a8f8b3963
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-08-19 18:31:35 +0530

    bin to int

---
+++ binary to int.py
@@ -0,0 +1,12 @@
+# n = 4
+# binary = format(n, 'b')
+# print(binary) # Output: '100'
+# binary = '100'
+# decimal = int(binary, 2)
+# print(decimal) # Output: 4
+
+a='11'
+b='1'
+c=int(a,2)+int(b,2)
+d=format(c,'b')
+print(str(d))
