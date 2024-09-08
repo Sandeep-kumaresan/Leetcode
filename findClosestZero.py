3526fe51ed8b9afcb1781b31cb499ff430df7b5a
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-09-08 19:34:26 +0530

    solution

--- findClosestZero.py
+++ findClosestZero.py
@@ -1,11 +1,21 @@
 #want to complete
-def findClosestNumber(nums):
-    min = 999999
+# def findClosestNumber(nums):
+#     min = 999999
+#     for i in nums:
+#         if abs(i) > 0 and abs(i) < abs(min) :
+#             min = i
+#         if abs(i) == 0:
+#             min = 0
+#     return min
+def findClosestNumber(self, nums):
+    CloseNum = nums[0]
+
     for i in nums:
-        if abs(i) > 0 and abs(i) < abs(min) :
-            min = i
-        if abs(i) == 0:
-            min = 0
-    return min
+        if abs(i) < abs(CloseNum):
+            CloseNum = i
+
+    if CloseNum < 0 and abs(CloseNum) in nums:
+        return abs(CloseNum)
+    return CloseNum
 nums = [-4, -2, 1, 4, 8]
 print(findClosestNumber(nums))
